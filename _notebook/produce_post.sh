68ddba2d586f39c03b02ea1f12ce2aec0368014c
levon003@beijing <levon003@umn.edu>
2019-10-05 17:37:38 -0500

    Changed post production process to automatically add header.

--- _notebook/produce_post.sh
+++ _notebook/produce_post.sh
@@ -4,14 +4,17 @@
 git_root_dir=$( git rev-parse --show-toplevel )
 nb_dir="${git_root_dir}/_notebook"
 input_filename="${1}"
-output_filename="$( basename -s .ipynb "${input_filename}" ).md"
+input_basename="$( basename -s .ipynb "${input_filename}" )"
+output_filename="$( date +%F )-${input_basename}.md"
 input_filepath="${nb_dir}/${input_filename}"
 output_filepath="${git_root_dir}/_posts/${output_filename}"
 
 echo "Converting '${input_filepath}'."
 jupyter nbconvert "${input_filepath}" --to markdown
-mv "${nb_dir}/${output_filename}" "${output_filepath}.tmp"
-grep -v '<IPython.core.display.Javascript object>' "${output_filepath}.tmp" > "${output_filepath}"
+mv "${nb_dir}/${input_basename}.md" "${output_filepath}.tmp"
+grep -v '<IPython.core.display.Javascript object>' "${output_filepath}.tmp" > "${output_filepath}.noheader"
+cat "${nb_dir}/${input_basename}.header" "${output_filepath}.noheader" > "${output_filepath}"
 rm "${output_filepath}.tmp"
+rm "${output_filepath}.noheader"
 echo "Finished. Post produced at '${output_filepath}'."
 
